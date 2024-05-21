1363ac260ec6a61f081a5d683ef84b3a5701cecc
Etienne Posthumus <eposthumus@gmail.com>
2024-05-21 19:39:15 +0200

    New version, no more sqlite

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ For more information see: http://www.iconclass.org/
 Made by Hans Brandhorst <jpjbrand@xs4all.nl> & Etienne Posthumus <eposthumus@gmail.com>
 
     ...with lots of support from a global cast of many, many people since 1972.
+
+## Example usage:
+
+```python
+from iconclass import init
+
+ic = init()
+a = ic["53A2"]
+
+print(a)
+print(repr(a))
+print(a())
+print(a('de'))
+
+for child in a:
+    print(child('it'))
+
+for p in a.path():
+    print(repr(p))
+```
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from iconclass import *
 
+### These tests are failing with the latest version, need to be updated
+
 # https://nose.readthedocs.org/en/latest/usage.html
 
 test_notations = ["0", "1", "25FF"]
@@ -15,14 +17,14 @@ def test_bogosity():
 
 def test_keywords():
     x = get("0")
-    assert x["kw"]["en"] == [u"abstract art", u"art", u"non-representational art"]
+    assert x["kw"]["en"] == ["abstract art", "art", "non-representational art"]
     x = get("31AA25")
-    assert x["kw"]["de"] == [u"Arm", u"Geste", u"Haltung", u"Hand"]
+    assert x["kw"]["de"] == ["Arm", "Geste", "Haltung", "Hand"]
 
 
 def test_keys():
     x = get("11(+1)")
-    assert x["txt"]["en"] == u"Christian religion (+ Holy Trinity)"
+    assert x["txt"]["en"] == "Christian religion (+ Holy Trinity)"
     assert x["c"] == ["11(+11)", "11(+12)", "11(+13)"]
     # handle a bogus key gracefully...
     x = get("25FF1(+8)")
@@ -34,7 +36,7 @@ def test_withnames():
     assert o["txt"]["en"] == "male saints (FOO)"
     assert (
         get("11H(FOO)0")["txt"]["en"]
-        == u"male saints (FOO) - male saint represented in a group"
+        == "male saints (FOO) - male saint represented in a group"
     )
 
 
@@ -45,7 +47,7 @@ def test_basic():
     assert results[1]["n"] == "1"
     assert results[1]["p"] == ["1"]
     assert results[2]["p"] == ["2", "25", "25F", "25FF"]
-    assert results[0]["txt"]["de"] == u"abstrakte, ungegenständliche Kunst"
+    assert results[0]["txt"]["de"] == "abstrakte, ungegenständliche Kunst"
     c = [
         "25FF1",
         "25FF2",
@@ -69,32 +71,32 @@ def test_basic():
 
 
 def test_spaces():
-    assert add_space(u"11H1") == u"11 H 1"
-    assert add_space(u"25F(+0)") == u"25 F (+0)"
-    assert add_space(u"25F1(+123)") == u"25 F 1 (+12 3)"
-    assert add_space(u"1") == u"1"
-    assert add_space(u"11") == u"11"
-    assert add_space(u"11H") == u"11 H"
-    assert add_space(u"11H(...)") == u"11 H (...)"
-    assert add_space(u"11H(JOHN)") == u"11 H (JOHN)"
-    assert add_space(u"25FF") == u"25 FF"
-    assert add_space(u"25F(+123)") == u"25 F (+12 3)"
-    assert add_space(u"11H(JOHN)(+12345)") == u"11 H (JOHN) (+12 34 5)"
-    assert add_space(u"7(+5)") == u"7 (+5)"
-    assert add_space(u"31AA") == u"31 AA"
-    assert add_space(u"31AA2(+0)") == u"31 AA 2 (+0)"
-    assert add_space(u"31AA25(+0)") == u"31 AA 25 (+0)"
-    assert add_space(u"11HH") == u"11 HH"
-    assert add_space(u"11HH(AGNES)") == u"11 HH (AGNES)"
-    assert add_space(u"11HH(AGNES)(+1)") == u"11 HH (AGNES) (+1)"
-    assert add_space(u"11HH(AGNES)3(+1)") == u"11 HH (AGNES) 3 (+1)"
-    assert add_space(u"11HH(AGNES)31(+31)") == u"11 HH (AGNES) 31 (+31)"
-    assert add_space(u"43C1(+411)") == u"43 C 1 (+41 1)"
-    assert add_space(u"43C78611(+3111)") == u"43 C 78 61 1 (+31 11)"
-    assert add_space(u"61B2(...)2(+5)") == u"61 B 2 (...) 2 (+5)"
-    assert add_space(u"34(+91)") == u"34 (+91)"
-    assert add_space(u"83(BOCCACCIO, Decamerone)") == u"83 (BOCCACCIO, Decamerone)"
-    assert add_space(u"11H(ALOYSIUS GONZAGA)") == u"11 H (ALOYSIUS GONZAGA)"
+    assert add_space("11H1") == "11 H 1"
+    assert add_space("25F(+0)") == "25 F (+0)"
+    assert add_space("25F1(+123)") == "25 F 1 (+12 3)"
+    assert add_space("1") == "1"
+    assert add_space("11") == "11"
+    assert add_space("11H") == "11 H"
+    assert add_space("11H(...)") == "11 H (...)"
+    assert add_space("11H(JOHN)") == "11 H (JOHN)"
+    assert add_space("25FF") == "25 FF"
+    assert add_space("25F(+123)") == "25 F (+12 3)"
+    assert add_space("11H(JOHN)(+12345)") == "11 H (JOHN) (+12 34 5)"
+    assert add_space("7(+5)") == "7 (+5)"
+    assert add_space("31AA") == "31 AA"
+    assert add_space("31AA2(+0)") == "31 AA 2 (+0)"
+    assert add_space("31AA25(+0)") == "31 AA 25 (+0)"
+    assert add_space("11HH") == "11 HH"
+    assert add_space("11HH(AGNES)") == "11 HH (AGNES)"
+    assert add_space("11HH(AGNES)(+1)") == "11 HH (AGNES) (+1)"
+    assert add_space("11HH(AGNES)3(+1)") == "11 HH (AGNES) 3 (+1)"
+    assert add_space("11HH(AGNES)31(+31)") == "11 HH (AGNES) 31 (+31)"
+    assert add_space("43C1(+411)") == "43 C 1 (+41 1)"
+    assert add_space("43C78611(+3111)") == "43 C 78 61 1 (+31 11)"
+    assert add_space("61B2(...)2(+5)") == "61 B 2 (...) 2 (+5)"
+    assert add_space("34(+91)") == "34 (+91)"
+    assert add_space("83(BOCCACCIO, Decamerone)") == "83 (BOCCACCIO, Decamerone)"
+    assert add_space("11H(ALOYSIUS GONZAGA)") == "11 H (ALOYSIUS GONZAGA)"
 
 
 if __name__ == "__main__":
